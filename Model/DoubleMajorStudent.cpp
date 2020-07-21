87de582088a3b1eed424b9f5c4cf97f6d04baab2
AmirHosseinHedayati <amir.0098.14@gmail.com>
2020-07-21 09:46:09 +0430

    improve class DoubleMajorStudent

--- Model/DoubleMajorStudent.cpp
+++ Model/DoubleMajorStudent.cpp
@@ -2,8 +2,8 @@
 
 
 DoubleMajorStudent::DoubleMajorStudent(const std::string& studentId, std::string first, std::string last , double workHours,
-                   std::vector<std::string> passedCourses, std::map<std::string, double> currentSemesterCourses)
-                   :Student(studentId,first,last,workHours,passedCourses,currentSemesterCourses)
+                   std::vector<std::string> passedCourses, std::map<std::string, double> currentSemesterCourses , std::string mj2)
+                   :Student(studentId,first,last,workHours,passedCourses,currentSemesterCourses) , major2 {mj2}
 {}
 
 double DoubleMajorStudent::calculateSalary() const{
--- Model/DoubleMajorStudent.h
+++ Model/DoubleMajorStudent.h
@@ -9,7 +9,7 @@ private:
 public:
     DoubleMajorStudent() : Student(){}
     DoubleMajorStudent(const std::string& studentId, std::string first, std::string last , double workHours,
-            std::vector<std::string> passedCourses, std::map<std::string, double> currentSemesterCourses);
+            std::vector<std::string> passedCourses, std::map<std::string, double> currentSemesterCourses, std::string mj2);
 
 
     virtual double calculateSalary() const override;
