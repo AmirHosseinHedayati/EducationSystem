4d807096a302e7d78a141a21342638ddd6300773
AmirHosseinHedayati <amir.0098.14@gmail.com>
2020-07-21 09:50:31 +0430

    improve class DoubleMajorStudent

--- Model/DoubleMajorStudent.h
+++ Model/DoubleMajorStudent.h
@@ -10,6 +10,8 @@ public:
     DoubleMajorStudent() : Student(){}
     DoubleMajorStudent(const std::string& studentId, std::string first, std::string last , double workHours,
             std::vector<std::string> passedCourses, std::map<std::string, double> currentSemesterCourses, std::string mj2);
+    DoubleMajorStudent(const std::string& studentId, std::string first, std::string last , double workHours,
+                       std::vector<std::string> passedCourses, std::map<std::string, double> currentSemesterCourses);
 
 
     virtual double calculateSalary() const override;
